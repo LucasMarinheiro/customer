326d3c191d8015cbd3596b06d3ca1f4fa571af6e
Lucas <lucassm1989@gmail.com>
2019-04-29 21:47:25 -0300

    mudar nome do endereco type address

--- src/main/java/com/register/customer/resources/TypeAddressResources.java
+++ src/main/java/com/register/customer/resources/TypeAddressResources.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/typeaddress")
+@RequestMapping("/type_address")
 public class TypeAddressResources {
     private final TypeAddressService typeAddressService;
 
